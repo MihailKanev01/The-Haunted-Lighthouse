099ed01a1c0cc3482ced6cab11bdd917513a8736
MihailKanev01 <mihail.kanev.1995@abv.bg>
2024-09-29 17:46:53 +0300

    Update
    Added door

--- Script/Player/player.gd
+++ Script/Player/player.gd
@@ -3,19 +3,35 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+@onready var jump_sound = $JumpSound
+var was_in_air = false
+@onready var landing_sound = $LandingSound
+var interact_range = 3.0
+var interact_ray = RayCast3D.new()
 
+func _ready():
+	add_child(interact_ray)
+	interact_ray.target_position = Vector3(0, 0, interact_range)
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	if Input.is_action_just_pressed("Interact"):
+		interact_ray.force_raycast_update()
+		if interact_ray.is_colliding():
+			var collider = interact_ray.get_collider()
+			if collider.has_meta("open door"):
+				collider.open_door()
+
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		was_in_air = true
+	elif was_in_air:
+		landing_sound.play()
+		was_in_air = false
 
-	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_sound.play()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("Left", "Right", "Forward", "Backwards")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
